d5bed2330301cc533c8c973e827b22f01e6d4a6c
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-06-16 12:06:48 -0300

    .

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = process.env.SUPABASE_URL ?? "";
-const SUPABASE_KEY = process.env.SUPABASE_KEY ?? "";
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "";
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
     schema: "public",
